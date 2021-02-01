4bae0f4c820ab8d0a18edf15252bdcec05859940
Calvin Carter <calvincarter@pingidentity.com>
2021-02-01 17:39:22 -0600

    first commit

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,19 +3,7 @@ const isAuthenticatedByJWT = require("../config/middleware/isAuthenticatedByJWT.
 module.exports = function (app) {
   // Route for getting some data about our user to be used client side
   /* Note the passport.authenticate('jwt') will run 1st and validate users JWT before giving access to user information */
-  app.post("/api/user_data", (req, res) => {
-
-    req.body.email
-    req.body.image
-    req.body.name
-
-    if(email) {
-      then navigate user inside our app
-    } else { // the user doesn't exist
-      create user in Db
-      then navigate user inside our app
-    }
-
+  app.get("/api/user_data", isAuthenticatedByJWT, (req, res) => {
     res.json({
       email: req.user.email,
       id: req.user.id,
